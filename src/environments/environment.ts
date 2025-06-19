e17c00daf438ba4ca02e5f9bfa79eec48068a4ae
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-06-18 23:43:11 -0400

    feat: Environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production : true,
+    companyName : 'Gifs',
+    companyNameShort : 'Apps',
+    companySlogan : 'Gifs para todos',
+
+    // Apikeys
+
+    // Endpoints
+
+    // Urls
+};
